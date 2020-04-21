5700c0219b9b1c163018eb266dc46c09ffe033b2
Cornel <filipcornelcristian@gmail.com>
2020-04-21 14:35:01 +0300

    emmiting custom events

--- src/Product.svelte
+++ src/Product.svelte
@@ -1,9 +1,17 @@
 <script>
-  export let productTitle 
+  import { createEventDispatcher } from "svelte";
+
+  export let productTitle;
+
+  const dispatch = createEventDispatcher();
+
+  const addToCart = () => {
+    dispatch("add-to-cart");
+  };
 </script>
 
 <article>
   <h1>{productTitle}</h1>
-  <button on:click>Add to cart</button>
-  <button>Delete</button>
+  <button on:click={addToCart}>Add to cart</button>
+  <button on:click={() => dispatch('delete')}>Delete</button>
 </article>
