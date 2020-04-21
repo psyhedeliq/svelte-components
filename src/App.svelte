67bfb8a1ad02aaadbe7f77281ee54ef5e2a5ac83
Cornel <filipcornelcristian@gmail.com>
2020-04-21 14:44:00 +0300

    extract data from custom events

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
   import Product from "./Product.svelte";
+
+  const addToCart = event => {
+    console.log(event);
+  };
+
+  const deleteProduct = event => {
+    console.log(event.detail);
+  };
 </script>
 
 <Product
   productTitle="A Book"
-  on:add-to-cart={() => alert('Added to cart!')}
-  on:delete={() => alert('Deleted!')} />
+  on:add-to-cart={addToCart}
+  on:delete={deleteProduct} />
